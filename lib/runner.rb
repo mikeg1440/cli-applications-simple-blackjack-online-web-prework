35edcbce20e9c61a20acd330d44d980899fe3d22
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-01 03:43:08 +0000

    Automatically backed up by Learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -1,19 +1,4 @@
 require_relative "blackjack.rb"
 
 
-welcome
-
-card_total = initial_round
-
-until card_total >= 21 do
-  
-  card_total += hit?(card_total)
-  
-  display_card_total(card_total)
-  
-end
-
-end_game
-
-
-#runner
+runner
