df59fb49d71dcbdaa111957b46ddaaaec4ec76b2
Michael Gaudreau <gaudreaum01@gmail.com>
2019-09-01 03:07:59 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,44 +1,72 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
 def deal_card
-  # code #deal_card here
+  rand 1..11
 end
 
-def display_card_total
-  # code #display_card_total here
+def display_card_total(total)
+  puts "Your cards add up to #{total}"
 end
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
+  
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+  display_card_total((deal_card+deal_card))
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  
+  resp = get_user_input
+  
+  if resp.downcase == "s"
+    return card_total
+  elsif resp.downcase == "h"
+    card_total += deal_card
+  else
+    puts "Please enter a valid command"
+  end
+  
+  card_total
 end
 
 def invalid_command
-  # code invalid_command here
+  prompt_user
 end
 
 #####################################################
 # get every test to pass before coding runner below #
 #####################################################
 
-def runner
-  # code runner here
+def runner1
+  
+  welcome
+  
+  card_total = initial_round
+  
+  
+  until card_total >= 21 do
+    
+    resp = prompt_user
+    
+    if resp.downcase == "h"
+      card_total += deal_card
+    end
+  
+  end  
+  
 end
     
